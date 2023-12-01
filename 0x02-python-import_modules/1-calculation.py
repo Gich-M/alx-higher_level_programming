12794baa5f1c63073086a936527e6ed4dee88bcf
Gich-M <gichmuriuki21@gmail.com>
2023-11-30 18:37:52 -0800

    variable_load

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    """Print sum, difference, multiple and division of 10 & 5."""
+    from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-print(f"{a} + {b} = {add(a, b)}")
-print(f"{a} - {b} = {sub(a, b)}")
-print(f"{a} * {b} = {mul(a, b)}")
-print(f"{a} / {b} = {div(a, b)}")
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
