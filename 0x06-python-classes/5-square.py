00077f1d9e989f4e9a91888300b3b071ef38cce0
Gich-M <gichmuriuki21@gmail.com>
2023-12-30 23:41:07 -0800

    # prints

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -15,13 +15,13 @@ class Square:
         self.__size = size
 
     @property
-    """Get the size of the square"""
     def size(self):
-        return self.__size
+        """Get the size of the square"""
+        return (self.__size)
 
     @size.setter
-    """Set size of the square"""
     def size(self, value):
+        """Set size of the square"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -33,6 +33,7 @@ class Square:
         return (self.__size ** 2)
 
     def my_print(self):
+        """Print square with the # character"""
         if self.__size == 0:
             print()
         else:
