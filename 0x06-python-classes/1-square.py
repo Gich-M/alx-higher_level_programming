06f11f2726f09a25d082587741584854391f22d7
Gich-M <gichmuriuki21@gmail.com>
2023-12-30 22:36:19 -0800

    new Square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
-"""Define a classs Square"""
+"""Define a class Square."""
+
 
 class Square:
-    """Represent a square"""
+    """Represent a square."""
 
     def __init__(self, size):
         """Initialize a new Square.
 
         Args:
             size (int): The size of the new square.
-            """
+        """
         self.__size = size
