74c207eb92146c8c22ab1e789e5cf1d2c507b426
Gich-M <gichmuriuki21@gmail.com>
2023-12-30 22:51:42 -0800

    Exception handling

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,7 +2,6 @@
 
 """Define a class Square."""
 
-
 class Square:
     """Represent a square."""
 
@@ -12,8 +11,8 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
