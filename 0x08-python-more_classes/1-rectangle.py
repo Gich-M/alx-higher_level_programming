da24affc90125b335e1780b83a168585aaca671b
Gich-M <gichmuriuki21@gmail.com>
2024-01-19 22:58:59 -0800

    Recs error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,9 +2,10 @@
 
 """Defines a class Rectangle."""
 
+
 class Rectangle:
     """Represents a rectangle."""
-    def __init__(self, width = 0, height=0):
+    def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
         Args:
@@ -21,7 +22,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance (value, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -34,7 +35,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance (value, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
