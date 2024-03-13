eb1e0ebb881093a37098bbc2a4bd2186a62b1171
gichmuriuki21@gmail.com <gichmuriuki21@gmail.com>
2024-03-14 02:34:10 +0300

    checker errors

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,13 +2,10 @@
 
 const integers = process.argv.slice(2).map(Number);
 
-const sorted  = integers.sort((a, b) => b - a);
+const sorted = integers.sort((a, b) => b - a);
 
-if (sorted.length < 2)
-{
-	console.log(0);
-}
-else
-{
-	console.log(sorted[1]);
+if (sorted.length < 2) {
+  console.log(0);
+} else {
+  console.log(sorted[1]);
 }
