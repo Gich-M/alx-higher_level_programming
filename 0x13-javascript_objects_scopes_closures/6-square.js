f6a19baed605358ccd87213069e1481cdca988fc
gichmuriuki21@gmail.com <gichmuriuki21@gmail.com>
2024-03-14 02:38:29 +0300

    objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,22 +2,18 @@
 
 const Square2 = require('./5-square');
 
-class Square extends Square2
-{
-	constructor(size)
-	{
-		super(size, size);
-	}
-	charPrint(c)
-	{
-		if (c === undefined)
-		{
-			c = 'X';
-		}
-		for (let i = 0; i < this.height; i++)
-		{
-			console.log(c.repeat(this.width));
-		}
-	}
+class Square extends Square2 {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
 module.exports = Square;
--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 
-exports.nbOccurences = function (list, searchElement)
-{
-	let count = 0;
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
 
-	for (let i = 0; i < list.length; i++)
-	{
-		if (list[i] === searchElement)
-		{
-			count++;
-		}
-	}
-	return count;
-}
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
