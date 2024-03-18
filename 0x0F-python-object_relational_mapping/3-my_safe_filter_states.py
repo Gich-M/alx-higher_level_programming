00c2bf324ad13989edb41fa8895719d7f81a64ba
Gich-M <gichmuriuki21@gmail.com>
2024-03-17 18:29:58 -0700

    Checker fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = conn.cursor()
-    arg = sys.argv[4]
-    curr.execute("SELECT * FROM states WHERE name LIKE %s", (arg, ))
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (arg, ))
     states = cur.fetchall()
     for state in states:
         print(state)
