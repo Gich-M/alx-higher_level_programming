5bc38ad39036a436ac6bb0c0fb29efa50f1e6e36
Gich-M <gichmuriuki21@gmail.com>
2024-07-29 06:55:42 -0700

    github commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,15 +10,19 @@
 """
 import requests
 import sys
-from requests.auth import HTTPBasicAuth
 
 if __name__ == '__main__':
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    res = requests.get(
-        "https://api.github.com/repos/{}/commits".format(sys.argv[3]),
-        auth=auth)
-    for commit in res.json():
-        print(
-            "{:s}: {:s}".format(
-                commit.get("sha"),
-                commit.get("commit").get("author").get("name")))
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    res = requests.get(url)
+
+    commits = res.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
