35897789ecc2ef77291647f54423046db9755ca9
Gich-M <gichmuriuki21@gmail.com>
2024-07-29 05:36:22 -0700

    You got me!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to `0.0.0.0:5000/catch_me that causes server to respond with body response message 'You got me!'.
+# Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing 'You got me!'.
 curl -s -X GET 0.0.0.0:5000/catch_me -w "\nYou got me!"
